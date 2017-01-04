861feab6259a1d30030ac43f2f002b5c34dbbdcd
RBFrame <rb_git@null.spigotmc.org>
2017-01-04 14:23:08 -0500

    Unnecessary long wait times
    Removed songs that ended in long wait times

--- js/songdata/songs.js
+++ js/songdata/songs.js
@@ -31,15 +31,6 @@ AddSong("Karma Fields","Edge Of The World","Electro","KarmaFields_EdgeOfTheWorld
 AddSong("Karma Fields","For Me","Electro","KarmaFields_ForMe.mp3","img/artists/karmafields.png","newagedarkage")
 AddSong("Karma Fields & MORTEN","Stickup (feat. Juliette Lewis)<br>(Modestep Remix)","Drumstep","KarmaFieldsAndMORTEN_StickupModestepRemix.mp3","img/artists/karmafields.png","stickupsingle")
 
-AddSong("Laszlo","A King's Life","Progressive House","Laszlo_AKingsLife.mp3","img/artists/laszlo.png","closerep")
-AddSong("Laszlo","Airglow","Future House","Laszlo_Airglow.mp3","img/artists/laszlo.png","airglowsingle")
-AddSong("Laszlo","Closer","EDM","Laszlo_Closer.mp3","img/artists/laszlo.png","closerep")
-AddSong("Laszlo","Don't Look Down","Drum & Bass","Laszlo_DontLookDown.mp3","img/artists/laszlo.png","dontlookdownsingle")
-AddSong("Laszlo","Fall To Light","Progressive House","Laszlo_FallToLight.mp3","img/artists/laszlo.png","falltolightsingle")
-AddSong("Laszlo","Here We Are","Drum & Bass","Laszlo_HereWeAre.mp3","img/artists/laszlo.png","herewearesingle")
-AddSong("Laszlo","Home<br>(feat. Richard Caddock)","Progressive House","Laszlo_Home.mp3","img/artists/laszlo.png","homesingle")
-AddSong("Laszlo X WRLD","You & Me","Future Bass","LaszloXWRLD_YouAndMe.mp3","img/artists/laszlo.png","youandmesingle")
-
 
 AddSong("Muzzy & Day One","Black Magic","Drum & Bass","MuzzyAndDayOne_BlackMagic.mp3","img/artists/muzzy.png","monstercat008anniversary")
 AddSong("Muzzy","Calling Out<br>(feat. KG & Skyelle)","Drum & Bass","Muzzy_CallingOut.mp3","img/artists/muzzy.png","callingoutsingle")
@@ -55,37 +46,9 @@ AddSong("Muzzy","The Phantom<br>(feat. High Maintenance)","Drum & Bass","Muzzy_T
 
 AddSong("POP ETC", "Your Heart Is A Weapon", "EDM", "07 - Your Heart Is a Weapon.mp3", "", "souvenir")
 
-AddSong("Puppet & Foria","I'm Here","Indie Dance","PuppetAndForia_ImHere.mp3","img/artists/puppet.png","imheresingle")
-AddSong("Puppet &<br>The Eden Project","The Fire","Electro","PuppetAndTheEdenProject_TheFire.mp3","img/artists/puppet.png","thefiresingle")
-
-AddSong("Rootkit","Against The Sun<br>(feat. Anna Yvette)","Drumstep","Rootkit_AgainstTheSun.mp3","img/artists/rootkit.png","againstthesunsingle")
-AddSong("Rootkit","Real Love<br>(feat. Danyka Nadeau)","Future House","Rootkit_RealLove.mp3","img/artists/rootkit.png","reallovesingle")
-
-AddSong("Rogue","Air","Dubstep","Rogue_Air.mp3","img/artists/rogue.png","dreamsep")
-AddSong("Rogue","Atlantic","Electro","Rogue_Atlantic.mp3","img/artists/rogue.png","atlanticsingle")
-AddSong("Rogue","Dreams<br>(feat. Laura Brehm)","Drumstep","Rogue_Dreams.mp3","img/artists/rogue.png","dreamsep")
-AddSong("Rogue","Drive Away","EDM","Rogue_DriveAway.mp3","img/artists/rogue.png","driveawaysingle")
-AddSong("Rogue","From The Dust","Drumstep","Rogue_FromTheDust.mp3","img/artists/rogue.png","earthep")
-AddSong("Rogue","Imperfect Views","EDM","Rogue_ImperfectViews.mp3","img/artists/rogue.png","monstercat5yearanniversary")
-AddSong("Rogue","Moments<br>(feat. Meg Dean)","House","Rogue_Moments.mp3","img/artists/rogue.png","dreamsep")
-AddSong("Rogue","Nightfall","Glitch Hop Or 110 BPM","Rogue_Nightfall.mp3","img/artists/rogue.png","monstercat012aftermath")
-AddSong("Rogue","Through The Dark","Electro","Rogue_ThroughTheDark.mp3","img/artists/rogue.png","earthep")
-AddSong("Rogue","Through The Dark","Electro","Rogue_ThroughTheDark.mp3","img/artists/rogue.png","earthep")
-
-AddSong("Tristam","Before We Fade","EDM","Tristam_BeforeWeFade.mp3","img/artists/tristam.png","monstercat5yearanniversary")
-AddSong("Tristam","Crave","Electro House","Tristam_Crave.mp3","img/artists/tristam.png","cravesingle")
-AddSong("Tristam & Braken","Flight","Drumstep","TristamAndBraken_Flight.mp3","img/artists/tristam.png","monstercat012aftermath")
-AddSong("Tristam & Braken","Frame Of Mind","Drum & Bass","TristamAndBraken_FrameOfMind.mp3","img/artists/tristam.png","frameofmindsingle")
-AddSong("Tristam","Once Again","Glitch Hop Or 110 BPM","Tristam_OnceAgain.mp3","img/artists/tristam.png","monstercat016expedition")
-AddSong("Tristam","Till It's Over","Glitch Hop Or 110 BPM","Tristam_TillItsOver.mp3","img/artists/tristam.png","tillitsoversingle")
-AddSong("Tristam","Truth","Electro","Tristam_Truth.mp3","img/artists/tristam.png","monstercat010conquest")
 
 AddSong("Tut Tut Child","Drink Up","Glitch Hop Or 110 BPM","TutTutChild_DrinkUp.mp3","img/artists/tuttutchild.png","drinkupsingle")
 AddSong("Tut Tut Child","Power Fracture","EDM","TutTutChild_PowerFracture.mp3","img/artists/tuttutchild.png","powerfracturesingle")
-AddSong("Tut Tut Child","The Uppity Strut","Glitch Hop Or 110 BPM","TutTutChild_TheUppityStrut.mp3","img/artists/tuttutchild.png","theuppitystrutsingle")
-
-AddSong("Vicetone","Heartbeat<br>(feat. Collin McLoughlin)","Progressive House","Vicetone_Heartbeat.mp3","img/artists/vicetone.png","monstercat012aftermath")
-AddSong("Vicetone","Nevada<br>(feat. Cozi Zuehlsdorff)","Progressive House","Vicetone_Nevada.mp3","img/artists/vicetone.png","nevadasingle")
 
 AddSong("Varien &<br>7 Minutes Dead","Mirai Sekai (Continuous Mix)","EDM","VarienAnd7MinutesDead_MiraiSekaiContinuousMix.mp3","img/artists/varien.png","miraisekaiep")
 AddSong("Varien","Valkyrie<br>(feat. Laura Brehm)","Drumstep","Varien_Valkyrie.mp3","img/artists/varien.png","valkyriesingle")
